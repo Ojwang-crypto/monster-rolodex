994c5d0bc5b8c075b1079d8b7b6f7ec19d7ef4c7
Ojwang-crypto <omondicollins163@gmail.com>
2023-03-21 18:12:11 +0300

    Added render and re-render methods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import './App.css';
-
+import CardList from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor(){
@@ -43,15 +43,10 @@ class App extends Component {
       <div className="App">
       
       <input className='search-box' type='search' placeholder='Search Monsters' onChange={this.onSearchChange} />
-      {filteredMonsters.map((monster) => {
-        return(
-          <div key={monster.id}>
-              <h1>{monster.name}</h1>
-          </div>
-        );
-      })}
+
+      < CardList monsters={filteredMonsters}/>
+      
      
-       
       </div>
     );
   }
